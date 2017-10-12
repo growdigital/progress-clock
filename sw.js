b3e5a6522dfd8549bec855067e54b6ab2abff829
Jake Rayson <web@growdigital.org>
2017-10-12 17:12:27 +0100

    added install steps for service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
 self.addEventListener('install', function(event) {
   // Perform install steps
+  var CACHE_NAME = 'progress-clock-cache-v1';
+  var urlsToCache = [
+    '/'
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
 });
